07994c23f7d6850a92b297b169728872aec3b8cf
Andrew Carey <andrew.r.carey@gmail.com>
2022-10-04 08:42:56 -0500

    completed all questions in part-supply.js

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -34,7 +34,8 @@ console.log(supplyChanges);
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.' 
 console.log('6. Showing supplyChanges...');
-for (x of supplyChanges){
+for (let i = 0; i < supplyChanges.length; i++) {
+    const x = supplyChanges[i]; 
     if (x>0) {
         console.log(`Added ${x} parts.`);
     } else if (x==0){
@@ -47,11 +48,27 @@ for (x of supplyChanges){
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
+// I actually wrote 6 as a 'for of' loop and had to copy it down to 7 and go back to write it as a for loop. 
 console.log('7. Showing supplyChanges with "for of" loop');
+for (x of supplyChanges){
+    if (x>0) {
+        console.log(`Added ${x} parts.`);
+    } else if (x==0){
+        console.log('No change.');
+    } else {
+        console.log(`Removed ${x*-1} parts.`); //logged the negative number as a positive to make the sentense make sense
+    }
+}
+
 
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
+let totalSupplies = 0
+for (x of supplyChanges) {
+    totalSupplies += x;
+}
+console.log(`The total number of supplies available is ${totalSupplies}.`);
 
 // 9. We have a large stash of parts in our warehouse that we 
 //    need to box up and get ready for shipment. 
@@ -60,3 +77,10 @@ console.log('8. Total supplies available is:');
 //    no more boxes can be filled.
 //    Then log how many boxes were filled, and how many parts are left over.
 console.log('9. Filling boxes with a "while" loop');
+let readyToBoxParts = 572
+let numberOfFilledBoxes = 0
+while (readyToBoxParts >= 7) {
+    readyToBoxParts -= 7;
+    numberOfFilledBoxes ++;
+}
+console.log(`There are ${numberOfFilledBoxes} boxes filled with ${readyToBoxParts} parts left over.`);
